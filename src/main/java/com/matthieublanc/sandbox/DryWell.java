65c795ddfbdf23763d77a026dab6f779f2edda42
mblanc <blanc.matthieu@gmail.com>
2013-06-10 18:33:25 +0200

    dryWell test

--- src/main/java/com/matthieublanc/sandbox/DryWell.java
+++ src/main/java/com/matthieublanc/sandbox/DryWell.java
@@ -2,21 +2,8 @@ package com.matthieublanc.sandbox;
 
 
 public class DryWell {
-	
-	public static int [] well = new  int[] {5, 6, 4, 3, 6, 2, 3};
-	public static int [] disks = new  int[] {2, 3, 5, 2, 4};
-	
-	public static int [] well2 = new  int[] {5, 6, 4, 3, 6, 2, 3};
-	public static int [] disks2 = new  int[] {6, 3, 5, 2, 4};
-	
-	
-	
-	public static void main(String[] args) {
-		System.out.println(falling_disks(well, disks));
-		System.out.println(falling_disks(well2, disks2));
-	}
 
-	public static int falling_disks(int[] well, int[] disks) {
+	public int solution(int[] well, int[] disks) {
 		int bottom = well.length;
 		for (int i = 0; i < disks.length; i++) {
 			int currentDisk =  disks[i];
